0a41305e63952ac4ea4eeba660447054e9655b06
sagar-6435 <sagarnaidu6435@gmail.com>
2026-05-02 19:57:04 +0530

    Remove gallery of smiles section from homepage

--- geethika-digital-world/client/src/pages/HomePage.jsx
+++ geethika-digital-world/client/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import FeaturesSection from '../components/FeaturesSection';
 import TrendingSection from '../components/TrendingSection';
 import ShopByOccasionSection from '../components/ShopByOccasionSection';
 import ProfessionalServicesSection from '../components/ProfessionalServicesSection';
-import GalleryHomeSection from '../components/GalleryHomeSection';
+
 import Testimonials from '../components/Testimonials';
 import CTASection from '../components/CTASection';
 
@@ -15,7 +15,7 @@ const HomePage = () => {
       <TrendingSection />
       <ShopByOccasionSection />
       <ProfessionalServicesSection />
-      <GalleryHomeSection />
+
       <Testimonials />
       <CTASection />
     </div>
